b2875c9c0a8718ec76a29cfc0ff75bf2ca0d4e3b
Ashutosh Kumar <ashutoshbokaro2003@gmail.com>
2026-02-20 13:14:21 +0530

    Prediction using decision tree algo on Social Network Ads dataset

---
+++ social_network_prediction.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('Social_Network_Ads.csv')
+
+data['Gender'].replace({"Male": 0, "Female": 1}, inplace=True)
+
+# print(data.head()) 
+x = data.iloc[:, 1:4].values
+y = data.iloc[:, -1].values
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+x = scaler.fit_transform(x) #as in age and estimated salary value there is a large difference so need to scale the values
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier()
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+print('Accuracy score:', accuracy_score(y_test, y_pred))
+# print(model.get_params())
+
+param_dist = {
+   "criterion": ["gini", "entropy"],
+   "max_depth": [1, 2, 3, 4, 5, 6, 7, None],
+}
+
+from sklearn.model_selection import GridSearchCV
+grid = GridSearchCV(model, param_grid=param_dist, cv=10, n_jobs = -1)
+
+grid.fit(x_train, y_train)
+
+print("best estimated :", grid.best_estimator_)
+print("params selected :", grid.best_params_)
+
+print("best score :", grid.best_score_)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+rn.metrics import ac
