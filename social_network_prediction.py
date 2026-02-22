db11c92bd3f8dae62d4956e3e00f1b022203d42c
Ashutosh Kumar <ashutoshbokaro2003@gmail.com>
2026-02-22 14:55:32 +0530

    Decision Tree REgressor Prediction

--- social_network_prediction.py
+++ social_network_prediction.py
@@ -6,7 +6,7 @@ data = pd.read_csv('Social_Network_Ads.csv')
 
 data['Gender'].replace({"Male": 0, "Female": 1}, inplace=True)
 
-# print(data.head()) 
+print(data.head()) 
 x = data.iloc[:, 1:4].values
 y = data.iloc[:, -1].values
 
@@ -18,18 +18,18 @@ x = scaler.fit_transform(x) #as in age and estimated salary value there is a lar
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
 
-from sklearn.tree import DecisionTreeClassifier
-model = DecisionTreeClassifier()
+from sklearn.tree import DecisionTreeRegressor
+model = DecisionTreeRegressor(max_depth=2, min_samples_split=2)
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
 from sklearn.metrics import accuracy_score
-print('Accuracy score:', accuracy_score(y_test, y_pred))
 # print(model.get_params())
 
 param_dist = {
-   "criterion": ["gini", "entropy"],
+   "criterion": ["squared_error", "friedman_mse", "absolute_error"],
    "max_depth": [1, 2, 3, 4, 5, 6, 7, None],
+   "min_samples_split": [2, 5, 10],
 }
 
 from sklearn.model_selection import GridSearchCV
@@ -37,7 +37,15 @@ grid = GridSearchCV(model, param_grid=param_dist, cv=10, n_jobs = -1)
 
 grid.fit(x_train, y_train)
 
+y_pred_binary = np.round(y_pred)
+print('Accuracy score:', accuracy_score(y_test, y_pred_binary))
+
 print("best estimated :", grid.best_estimator_)
 print("params selected :", grid.best_params_)
 
 print("best score :", grid.best_score_)
+
+from sklearn.tree import plot_tree
+plt.figure(figsize=(8,5))
+plot_tree(model, feature_names=['Gender', 'Age', 'Salary'], filled=True)
+plt.show()
